e94a5136baabe8e8b27822526e5776b47dc3325d
David Chan <davidlechan@gmail.com>
2026-03-07 15:36:02 -0800

    added browser-open to local start script

--- start-local.sh
+++ start-local.sh
@@ -1,5 +1,12 @@
 # run this and the local development server should start (in theory)
+
 rm -v *.log
 bundle install
+
+# open up the local build page in a browser
+nohup /run/current-system/sw/bin/firefox -p dchan-personal -new-window http://127.0.0.1:4000 &!
+
 # bundle exec jekyll serve --host 127.0.1 --port 4000 --trace --verbose | tee serve.log
 bundle exec jekyll serve --host 127.0.1 --port 4000 --livereload | tee serve.log
+
+
