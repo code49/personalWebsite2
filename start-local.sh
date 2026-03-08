1cf34e8f19f853b0f51e79bab19343b3d91ea444
David Chan <davidlechan@gmail.com>
2026-03-07 19:26:57 -0500

    disabled auto-browser (it apparently breaks auto reload), footer changes

--- start-local.sh
+++ start-local.sh
@@ -4,7 +4,7 @@ rm -v *.log
 bundle install
 
 # open up the local build page in a browser
-nohup /run/current-system/sw/bin/firefox -p dchan-personal -new-window http://127.0.0.1:4000
+# nohup /run/current-system/sw/bin/firefox -p dchan-personal -new-window http://127.0.0.1:4000
 
 # bundle exec jekyll serve --host 127.0.1 --port 4000 --trace --verbose | tee serve.log
 bundle exec jekyll serve --host 127.0.1 --port 4000 --livereload | tee serve.log
